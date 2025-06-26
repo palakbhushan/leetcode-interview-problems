dace2eeb44e56acc5026e4ae3bc4055f80bff5d4
Palak Bhushan <63420799+palakbhushan@users.noreply.github.com>
2025-06-26 22:31:30 +0530

    Time: 0 ms (100.00%) | Memory: 41.3 MB (46.85%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.java
+++ 119-pascals-triangle-ii/pascals-triangle-ii.java
@@ -1,22 +1,36 @@
 class Solution {
     public List<Integer> getRow(int rowIndex) {
-        List<List<Integer>> mainArr = new ArrayList<>();
-        List<Integer> arr = new ArrayList<>();
-        for(int i=0; i<=rowIndex; i++){
-            arr = new ArrayList<>();
-            for(int j=0; j<=i; j++){
-                if(j==0 || i==j || i<=1){
-                    arr.add(1);
-                } else {
-                    arr.add(mainArr.get(i-1).get(j-1) + mainArr.get(i-1).get(j));
-                }
-            }
+        // List<List<Integer>> mainArr = new ArrayList<>();
+        // List<Integer> arr = new ArrayList<>();
+        // for(int i=0; i<=rowIndex; i++){
+        //     arr = new ArrayList<>();
+        //     for(int j=0; j<=i; j++){
+        //         if(j==0 || i==j || i<=1){
+        //             arr.add(1);
+        //         } else {
+        //             arr.add(mainArr.get(i-1).get(j-1) + mainArr.get(i-1).get(j));
+        //         }
+        //     }
 
-            if(arr.size() > 0){
-                mainArr.add(arr);
-            }
+        //     if(arr.size() > 0){
+        //         mainArr.add(arr);
+        //     }
+        // }
+
+        // return arr;
+
+        List<Integer> arr = new ArrayList<>();
+        arr.add(1);
+        long val = 1;
+        for(int i=1; i<=rowIndex; i++){
+            val = val * (rowIndex - i + 1)/i;
+            arr.add((int) val);
         }
 
+        //using binomial coefficients
+
         return arr;
+
+        
     }
 }
