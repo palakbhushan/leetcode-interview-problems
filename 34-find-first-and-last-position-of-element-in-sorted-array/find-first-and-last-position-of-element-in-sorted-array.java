cf40701d796b9def1e4b1c60689fbdbbbeb54934
Palak Bhushan <63420799+palakbhushan@users.noreply.github.com>
2025-07-12 16:08:20 +0530

    Time: 0 ms (100.00%) | Memory: 45.9 MB (68.01%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,52 +1,33 @@
 class Solution {
     public int[] searchRange(int[] nums, int target) {
-        
-        int n = nums.length;
-        int low = 0;
-        int high = n - 1;
-        int lb = 0 , ub = n-1;
+        int[] arr = {-1, -1};
+        arr[0] = lowerAndUpperBound(nums, target, true);
+        arr[1] = lowerAndUpperBound(nums, target, false);
 
-        if(n == 0){
-            return new int[] {-1, -1};
-        } else if(n == 1){
-            if(nums[0] == target){
-                return new int[] {0, 0};
-            }
-        }
+        return arr;
 
+    }
 
+    static int lowerAndUpperBound(int nums[], int target, boolean isLowerBound){
+        int low = 0, high = nums.length - 1, ans = -1;
         while(low <= high){
-            int mid = (low + high)/2;
-            if(nums[mid] >= target){
-                lb = mid;
+            int mid = low + (high - low)/2;
+    
+            if(nums[mid] > target) {
                 high = mid -1;
-            } else {
+            } else if(nums[mid] < target) {
                 low = mid + 1;
-            }
-        }
-
-        low = 0;
-        high = n-1;
-
-        while(low <= high){
-            int mid = (low + high)/2;
-            if(nums[mid] <= target){
-                ub = mid;
-                low = mid  + 1;
             } else {
-                high = mid - 1;
+                ans = mid;
+
+                if(isLowerBound) {
+                    high = mid - 1;
+                } else {
+                    low = mid + 1;
+                }
             }
         }
 
-        if(nums[lb] != target){
-            lb = -1;
-        }
-
-        if(nums[ub] != target){
-            ub = -1;
-        }
-
-        return new int[] {lb, ub};
-
+        return ans;
     }
 }
