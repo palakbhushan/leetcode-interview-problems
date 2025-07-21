e129f8884153e3f894147cf55e63376fe6cfead0
Palak Bhushan <63420799+palakbhushan@users.noreply.github.com>
2025-07-21 21:55:54 +0530

    Time: 2 ms (25.86%) | Memory: 57.1 MB (90.56%) - LeetSync

--- 189-rotate-array/rotate-array.java
+++ 189-rotate-array/rotate-array.java
@@ -1,29 +1,28 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        
+        // Map<Integer, Integer> map = new HashMap<>();
         int n = nums.length;
-        Map<Integer, Integer> map = new HashMap<>();
         k = k % n;
 
+        int[] arr = new int[n];
 
-        for (int i = 0; i < n; i++) {
-            map.put((i + k) % n, nums[i]);
-        }
 
-        for (int i = 0; i < n; i++) {
-            nums[i] = map.get(i);
-        }
-
-        // for(int i=0; i<n; i++){
-        //     int indexToUpdate = i + k + 1;
-        //     if(indexToUpdate >= n){
-        //         indexToUpdate = indexToUpdate - n;
-        //     }
-
-        //     map.put(i, nums[i]);
-        //     nums[i] = map.get(indexToUpdate) != null ? map.get(indexToUpdate) : nums[indexToUpdate];
-
-            
+        // for (int i = 0; i < n; i++) {
+        //     map.put((i + k) % n, nums[i]);
         // }
+
+        // for (int i = 0; i < n; i++) {
+        //     nums[i] = map.get(i);
+        // }
+
+        for (int i = 0; i < n; i++) {
+            arr[(i+k) % n] = nums[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            nums[i] = arr[i];
+        }
+
+        
     }
 }
