cbfafc1e1a859a1520779d0a38d6385885f2deab
Palak Bhushan <63420799+palakbhushan@users.noreply.github.com>
2025-07-28 13:28:04 +0530

    Time: 0 ms (100.00%) | Memory: 50.6 MB (29.88%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,21 +1,44 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
         int low = 0, high = nums.length - 1;
-        while (low < high) {
-            int mid = low + (high - low) / 2;
 
-            if (mid % 2 == 1) {
-                mid--;
+
+        while(low<high){
+
+            int mid = low + (high - low)/2;
+
+            if(mid % 2 == 1){
+                mid --;
             }
 
-            if (nums[mid] == nums[mid + 1]) {
-                //single element idhar exists krta hai
+            if(nums[mid] == nums[mid+1]){
                 low = mid + 2;
             } else {
                 high = mid;
             }
+
         }
 
         return nums[low];
     }
 }
+
+// [1,1,2,3,3,4,4,8,8]
+
+// low = 0
+// high = n-1 = 8
+
+// mid = (0+8)/2 == 4
+
+// arr[mid] != arr[mid+1] && arr[mid] != arr[mid-1] // arr[mid] is a single element in an array
+
+// if(arr[mid] != arr[mid -1]){
+//     high = mid - 1;
+// } else {
+//     high = mid + 1;
+// }
+
+// [1,1,2,3,3,4,4,8,8]
+// [0,1,2,3,4,5,6,7,8]
+
+
