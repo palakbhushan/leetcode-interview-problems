fba21b161a428d936d990b43670b95f434c404d4
Palak Bhushan <63420799+palakbhushan@users.noreply.github.com>
2025-09-25 20:27:49 +0530

    Time: 13 ms (28.92%) | Memory: 50.1 MB (73.38%) - LeetSync

--- 169-majority-element/majority-element.java
+++ 169-majority-element/majority-element.java
@@ -1,22 +1,20 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<Integer, Integer>();
-
-        for(int i=0;i<nums.length;i++){
-            map.put(nums[i], map.getOrDefault(nums[i], 0) + 1);
+        HashMap<Integer, Integer> map = new HashMap<Integer,Integer>();
+        
+        for(int i=0; i<nums.length; i++){
+            map.put(nums[i], map.getOrDefault(nums[i] ,0) + 1);
         }
 
-        int cond = (int)Math.ceil(nums.length / 2.0);
+        
+        int halfCount = (int) Math.ceil(nums.length / 2.0);
+       
         int maxElem = Integer.MIN_VALUE;
-        System.out.println(nums.length / 2);
 
         for(Map.Entry<Integer, Integer> ent: map.entrySet()){
-            System.out.println("Value: " + ent.getValue() + ", Key: " + ent.getKey());
-        }
 
-        for(Map.Entry<Integer, Integer> ent: map.entrySet()){
-            if (ent.getValue() >= cond && maxElem < ent.getKey()){
-                maxElem = ent.getKey();
+            if(ent.getValue() >= halfCount ){
+                    maxElem = ent.getKey();
             }
         }
 
