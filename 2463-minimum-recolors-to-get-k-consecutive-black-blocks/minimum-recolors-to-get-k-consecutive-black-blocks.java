8da66676ddfa93c1f780619a816662ba661c1aa7
Palak Bhushan <63420799+palakbhushan@users.noreply.github.com>
2026-06-05 01:44:04 +0530

    Time: 1 ms (93.86%) | Memory: 42.9 MB (45.70%) - LeetSync

--- 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
+++ 2463-minimum-recolors-to-get-k-consecutive-black-blocks/minimum-recolors-to-get-k-consecutive-black-blocks.java
@@ -1,38 +1,28 @@
 class Solution {
     public int minimumRecolors(String blocks, int k) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        arr.add(0,0);
-        int maximum = 0;
+        int minWhiteBalls = 0;
+        int minWhiteBallsInWindow = 0;
 
-        //using pre computation tecnique here
-        for(int i =0; i< blocks.length(); i++){
-
-            // this one is for index 0 and also it consists B
-            if(i==0 && blocks.charAt(i) == 'B'){
-                arr.add(i,1);
-            }
-            // now checking if current val is B then add 1 in previous index value and insert in current space
-            if(blocks.charAt(i) == 'B' && i!=0){
-                arr.add(i, arr.get(i-1) + 1);
-            }
-
-            // if it is W then current count is same as previous one
-            if(blocks.charAt(i) == 'W' && i!=0) {
-                arr.add(i,arr.get(i -1));
-            }
-
-            if(i >= (k-1)){
-                
-                // now checking if window size is as same as required K then finding the maximum B count because maximum the B count minimum the recolor
-                if(i-k >= 0){
-                    maximum = Math.max(maximum, arr.get(i) - arr.get(i-k));
-                } else {
-                    maximum =  Math.max(maximum, arr.get(i));
+        for(int i=0; i<k; i++){
+            if(blocks.charAt(i) == 'W') {
+                minWhiteBalls++; 
+                minWhiteBallsInWindow++;
                 }
-                
-            }
         }
 
-        return k-maximum;
+        for(int i=k; i< blocks.length(); i++){
+
+            if(blocks.charAt(i - k) == 'W'){
+                minWhiteBallsInWindow--;
+            }
+
+            if(blocks.charAt(i) == 'W'){
+                minWhiteBallsInWindow++;
+            }
+            
+            minWhiteBalls = Math.min(minWhiteBalls, minWhiteBallsInWindow);
+        }
+
+        return minWhiteBalls;
     }
 }
